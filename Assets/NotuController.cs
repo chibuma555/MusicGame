40834a54ce1417807a0efbde8eb43ed8b3928cec
chibuma555 <82083675+chibuma555@users.noreply.github.com>
2021-05-27 21:24:54 +0900

    アイテムの生成(失敗）
    特になし

--- Assets/NotuController.cs
+++ Assets/NotuController.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class NotuController : MonoBehaviour
 {
-    Rigidbody myRigidvody;
+
     private float velocityX = 0f;
-    private float velocityY = 0.3f;
-    private float velocityZ = -15f;
+    private float velocityY = 0f;
+    private float velocityZ = -1f;
 
     // Start is called before the first frame update
     void Start()
     {
-        myRigidvody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
-      
-    }
-
-    void FixedUpdate()
-    {
-        myRigidvody.velocity = new Vector3(velocityX, velocityY, velocityZ);
+        transform.position += new Vector3(velocityX, velocityY, velocityZ);
     }
 }
 
--- Assets/NotuGenerator.cs
+++ Assets/NotuGenerator.cs
@@ -5,12 +5,45 @@ using UnityEngine;
 public class NotuGenerator : MonoBehaviour
 {
     // オブジェクトを入れる
-    public GameObject notu;
+    public GameObject notuPrefab;
+    // ノーツ生成地点(開始）
+    private int startPos = 100;
+    //ノーツ生成地点(終了）
+    private int goalPos = 360;
+    //ノーツのx軸の範囲
+    private float posRange = 2.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        float offsetZ = 1f;
+        GameObject notu;
+
+        // ノーツの生成する量を決める
+        for (int i = startPos; i < goalPos; i+=20)
+        {
+            //ノーツ1つ配置
+            notu = Instantiate(notuPrefab);
+            notu.transform.position = new Vector3();
+
+            //　ノーツ4つ配置
+            for (int j = -1; j <= 2; j++)
+            { 
+                notu = Instantiate(notuPrefab);
+                notu.transform.position = new Vector3(posRange * j, notu.transform.position.y * -1.5f, offsetZ * i);
+            }
+            // ノーツが3つ配置
+            for (int j = -1; j <= 1; j++)
+            {
+
+            }
+            //　ノーツを２つ配置
+            for (int j = -1; j <= 0; j++)
+            {
+
+            }
+
+        }
     }
 
     // Update is called once per frame
@@ -18,11 +51,4 @@ public class NotuGenerator : MonoBehaviour
     {
         
     }
-
-    /*
-    void OnTriggerEnter(Collider other)
-    {
-        Destroy(this.gameObject);
-    }
-    */
 }
