9a411f68c00d111bb2d65912cdcb50b07fdb9fb7
chibuma555 <82083675+chibuma555@users.noreply.github.com>
2021-06-03 18:30:23 +0900

    6/3

--- Assets/NotuController.cs
+++ Assets/NotuController.cs
@@ -4,21 +4,28 @@ using UnityEngine;
 
 public class NotuController : MonoBehaviour
 {
-
+   
     private float velocityX = 0f;
     private float velocityY = 0f;
-    private float velocityZ = -1f;
-
+    private float velocityZ = -0.7f;
+   
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(velocityX, velocityY, velocityZ);
+        //ノーツの移動
+        transform.position += new Vector3(velocityX, velocityY, velocityZ);       
     }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("あたったぞ ！！！");
+    }
+
+
 }
 
--- Assets/NotuGenerator.cs
+++ Assets/NotuGenerator.cs
@@ -4,47 +4,68 @@ using UnityEngine;
 
 public class NotuGenerator : MonoBehaviour
 {
-    // オブジェクトを入れる
-    public GameObject notuPrefab;
-    // ノーツ生成地点(開始）
-    private int startPos = 100;
-    //ノーツ生成地点(終了）
-    private int goalPos = 360;
-    //ノーツのx軸の範囲
-    private float posRange = 2.5f;
+
+    //オブジェクトを定義
+    public GameObject NotuPrefab;
+    //リストを定義
+    List<GameObject> ListValue = new List<GameObject>();
+
+    float xpos_1 = -3.7f;
+    float xpos_2 = -1.3f;
+    float xpos_3 = 1.2f;
+    float xpos_4 = 3.6f;
 
     // Start is called before the first frame update
     void Start()
     {
-        float offsetZ = 1f;
-        GameObject notu;
-
-        // ノーツの生成する量を決める
-        for (int i = startPos; i < goalPos; i+=20)
+        for(int i =0; i <= 30; i++)
         {
-            //ノーツ1つ配置
-            notu = Instantiate(notuPrefab);
-            notu.transform.position = new Vector3();
-
-            //　ノーツ4つ配置
-            for (int j = -1; j <= 2; j++)
-            { 
-                notu = Instantiate(notuPrefab);
-                notu.transform.position = new Vector3(posRange * j, notu.transform.position.y * -1.5f, offsetZ * i);
-            }
-            // ノーツが3つ配置
-            for (int j = -1; j <= 1; j++)
-            {
-
-            }
-            //　ノーツを２つ配置
-            for (int j = -1; j <= 0; j++)
-            {
-
-            }
-
+            GameObject notu = Instantiate(NotuPrefab);
+            ListValue.Add(notu);
         }
+
+        ListValue[0].transform.position = new Vector3(xpos_1, 1, 130f);
+        ListValue[1].transform.position = new Vector3(xpos_4, 1, 130f);
+        
+        ListValue[2].transform.position = new Vector3(xpos_1, 1, 200f);
+        ListValue[3].transform.position = new Vector3(xpos_4, 1, 200f);
+
+        ListValue[4].transform.position = new Vector3(xpos_1, 1, 250f);
+        ListValue[5].transform.position = new Vector3(xpos_4, 1, 300f);
+        ListValue[6].transform.position = new Vector3(xpos_4, 1, 350f);
+        ListValue[7].transform.position = new Vector3(xpos_3, 1, 400f);
+        ListValue[8].transform.position = new Vector3(xpos_1, 1, 500f);
+        ListValue[9].transform.position = new Vector3(xpos_2, 1, 550f);
+        ListValue[10].transform.position = new Vector3(xpos_1, 1, 600f);
+        ListValue[11].transform.position = new Vector3(xpos_3, 1, 600f);
+
+        ListValue[12].transform.position = new Vector3(xpos_2, 1, 650f);
+        ListValue[13].transform.position = new Vector3(xpos_3, 1, 650f);
+
+        ListValue[14].transform.position = new Vector3(xpos_1, 1, 700f);
+        ListValue[15].transform.position = new Vector3(xpos_2, 1, 710f);
+        ListValue[16].transform.position = new Vector3(xpos_1, 1, 720f);
+
+        ListValue[17].transform.position = new Vector3(xpos_2, 1, 750f);
+        ListValue[18].transform.position = new Vector3(xpos_4, 1, 750f);
+        ListValue[19].transform.position = new Vector3(xpos_3, 1, 770f);
+
+        ListValue[20].transform.position = new Vector3(xpos_1, 1, 800f);
+        ListValue[21].transform.position = new Vector3(xpos_4, 1, 800f);
+
+        ListValue[22].transform.position = new Vector3(xpos_2, 1, 850f);
+        ListValue[23].transform.position = new Vector3(xpos_3, 1, 850f);
+
+        ListValue[24].transform.position = new Vector3(xpos_4, 1, 900f);
+        ListValue[25].transform.position = new Vector3(xpos_1, 1, 930f);
+        ListValue[26].transform.position = new Vector3(xpos_2, 1, 950f);
+        ListValue[27].transform.position = new Vector3(xpos_1, 1, 980f);
+        ListValue[28].transform.position = new Vector3(xpos_2, 1, 990f);
+
+        ListValue[29].transform.position = new Vector3(xpos_1, 1, 1200f);
+        ListValue[30].transform.position = new Vector3(xpos_4, 1, 1200f);
     }
+
 
     // Update is called once per frame
     void Update()
